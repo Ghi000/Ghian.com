0a2de9bc93b9d01cd51b79aec2bbdbc75a626683
Ghi000 <153405810+Ghi000@users.noreply.github.com>
2023-12-11 23:48:53 +0800

    for you

---
+++ script.js
@@ -0,0 +1,23 @@
+const wrapper = document.querySelector(".wrapper");
+const question = document.querySelector(".question");
+const gif = document.querySelector(".gif");
+const yesBtn = document.querySelector(".yes-btn");
+const noBtn = document.querySelector(".no-btn");
+
+yesBtn.addEventListener("click", () => {
+  question.innerHTML = "Aaaaaa That I miss you to so muchhh";
+  gif.src =
+    "https://github.com/Ghi000/openit.com/assets/153405810/45cdae77-baf6-455a-a648-2324f429e276";
+});
+
+noBtn.addEventListener("mouseover", () => {
+  const noBtnRect = noBtn.getBoundingClientRect();
+  const maxX = window.innerWidth - noBtnRect.width;
+  const maxY = window.innerHeight - noBtnRect.height;
+
+  const randomX = Math.floor(Math.random() * maxX);
+  const randomY = Math.floor(Math.random() * maxY);
+
+  noBtn.style.left = randomX + "px";
+  noBtn.style.top = randomY + "px";
+});
